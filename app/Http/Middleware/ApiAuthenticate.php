2e69febe9c727c7f119bd6c72a3b12f41506355a
Jeremie Elbaz <jeremie@MacBook-Pro-de-Jeremie.local>
2017-12-14 10:34:57 +0100

    correction

--- app/Http/Middleware/ApiAuthenticate.php
+++ app/Http/Middleware/ApiAuthenticate.php
@@ -18,8 +18,8 @@ class ApiAuthenticate
         /**
          * Autorization
          */
-        header("Access-Control-Allow-Origin: *");
         $headers = [
+            'Access-Control-Allow-Origin' =>  '*',
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Id, Accept, X-Request-With, Authorization'
         ];
