a4dad154707b6e597c861145360c9f1d228cd0b9
Jeremie Elbaz <jeremie@MacBook-Pro-de-Jeremie.local>
2017-12-19 16:23:29 +0100

    ajout userinterface + api

--- app/Http/Middleware/ApiAuthenticate.php
+++ app/Http/Middleware/ApiAuthenticate.php
@@ -24,15 +24,16 @@ class ApiAuthenticate
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Id, Accept, X-Request-With, Authorization'
         ];
+        if ($request->getMethod() === "OPTIONS") {
+            return response('', 200, $headers);
+        }
         $response = $next($request);
         foreach ($headers as $key => $value)
             $response->header($key, $value);
         if ($request->header('authorization') == 'Bearer '.env('TOKEN', 'smartorder_token')) {
             return $response;
         }
-        if ($request->getMethod() == "OPTIONS") {
-            return $response;
-        }
+
 
 
         return response()->json(['error' => 'Authorization false'], 403, $headers);
